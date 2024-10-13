cf74a1c0318dae0b8fed0d85bcbb976e3c08b458
nicosleax <forceeax@gmail.com>
2024-10-13 11:54:56 +0200

    Last Details

--- src/main/java/de/phoenix_interactive_studios/spigot/Main.java
+++ src/main/java/de/phoenix_interactive_studios/spigot/Main.java
@@ -1,7 +1,26 @@
 package de.phoenix_interactive_studios.spigot;
 
-public class Main {
+import de.phoenix_interactive_studios.spigot.commands.Gamemode_Adventure;
+import de.phoenix_interactive_studios.spigot.commands.Gamemode_Creative;
+import de.phoenix_interactive_studios.spigot.commands.Gamemode_Spectator;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import de.phoenix_interactive_studios.spigot.commands.Gamemode_Survival;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    getConfig().options().copyDefaults();
+    saveDefaultConfig();
+
+    getCommand("gms").setExecutor(new Gamemode_Survival(this));
+    getCommand("gmc").setExecutor(new Gamemode_Creative(this));
+    getCommand("gmsp").setExecutor(new Gamemode_Spectator(this));
+    getCommand("gma").setExecutor(new Gamemode_Adventure(this));
 
 
+    }
 
 }
