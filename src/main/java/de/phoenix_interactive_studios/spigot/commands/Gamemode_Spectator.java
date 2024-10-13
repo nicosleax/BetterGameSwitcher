e66923d77a1c00438ab5ff3269d061439928d7ac
nicosleax <forceeax@gmail.com>
2024-10-13 11:54:19 +0200

    Created Gamemode_Spectator

---
+++ src/main/java/de/phoenix_interactive_studios/spigot/commands/Gamemode_Spectator.java
@@ -0,0 +1,33 @@
+package de.phoenix_interactive_studios.spigot.commands;
+
+import de.phoenix_interactive_studios.spigot.Main;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Gamemode_Spectator implements CommandExecutor {
+    private Main main;
+
+    public Gamemode_Spectator(Main main){
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+
+        if (sender instanceof Player) {
+
+            Player pl = (Player) sender;
+
+            if (pl.getGameMode() == GameMode.SPECTATOR) {
+                pl.sendMessage(main.getConfig().getString("Prefix") + main.getConfig().getString("Already_Spectator"));
+            } else {
+                pl.setGameMode(GameMode.SPECTATOR);
+                pl.sendMessage(main.getConfig().getString("Prefix") + main.getConfig().getString("Set_Spectator"));
+            }
+        }
+        return false;
+    }
+}
